56c600fbf32d773628e7b05b2f916745b0321e2c
Lucas Novaes <lucasnovaesdesaferreira@gmail.com>
2025-03-31 22:23:50 -0300

    uploading code

--- NossoVetor.java
+++ NossoVetor.java
@@ -1,10 +1,13 @@
+// import java.util.Calendar;
+
 import java.util.Random;
 
-public class NossoVetor{
+public class NossoVetor {
+
     private int[] vetor;
     private int ocupacao;
-    
-    public NossoVetor(int tamanho){
+
+    public NossoVetor(int tamanho) {
         vetor = new int[tamanho];
         ocupacao = 0;
     }
@@ -22,62 +25,88 @@ public class NossoVetor{
         ocupacao = vetor.length;
     }
 
-    public void bubbleSort(){
-        for (int i=1; i<vetor.length; i++){
-            for (int j=0; j<vetor.length-i; j++){
-                if (vetor[j]>vetor[j+1]){
+    public long bubbleSort() {
+        long contadorBS = 0;
+        for (int i = 1; i < vetor.length; i++) {
+            for (int j = 0; j < vetor.length - i; j++) {
+                if (vetor[j] > vetor[j + 1]) {
                     int aux = vetor[j];
-                    vetor[j] = vetor[j+1];
-                    vetor[j+1] = aux;
+                    vetor[j] = vetor[j + 1];
+                    vetor[j + 1] = aux;
+
+                    contadorBS++;
                 }
             }
         }
+        return contadorBS;
     }
 
-    public void selectionSort(){
-        for (int i=0; i<vetor.length-1; ++i){
+    public long selectionSort() {
+        long contadorSS = 0;
+        for (int i = 0; i < vetor.length - 1; ++i) {
             int min = i;
-            for (int j=i+1; j<vetor.length; ++j){
-                if (vetor[j]<vetor[min])
-                    min=j;
-            int x = vetor[i];
-            vetor[i] = vetor[min];
-            vetor[min] = x;
+            for (int j = i + 1; j < vetor.length; ++j) {
+                if (vetor[j] < vetor[min]) {
+                    min = j;
+                }
+                int x = vetor[i];
+                vetor[i] = vetor[min];
+                vetor[min] = x;
+                if (vetor[i] != vetor[min]) {
+                    contadorSS++;
+                }
+
             }
         }
+        return contadorSS;
     }
 
-    public void insertionSort(){
-        for (int j=1; j<vetor.length; ++j){
+    public long insertionSort() {
+        long contadorIS = 0;
+        for (int j = 1; j < vetor.length; ++j) {
             int x = vetor[j];
             int i;
-            for (i=j-1; i>=0&&vetor[i]>x; --i)
-                vetor[i+1] = vetor[i];
-            vetor[i+1]=x;
+            for (i = j - 1; i >= 0 && vetor[i] > x; --i) {
+                vetor[i + 1] = vetor[i];
+                contadorIS++;
+            }
+            vetor[i + 1] = x;
         }
+
+        return contadorIS;
     }
 
-    public int buscaLinear(int elemento){
-        for(int i = 0; i < ocupacao; i++){
-            if(vetor[i] == elemento){
-                return i;
+    public int buscaLinear(int elemento) {
+        int contadorLinear = 0;
+
+        for (int i = 0; i < vetor.length; i++) {
+            if (vetor[i] == elemento) {
+                // System.out.println(i); 
+
+                contadorLinear++;
             }
         }
-        return -1;
+        return contadorLinear;
     }
-    
-    public int buscaBinaria(int elemento){
+
+    public int buscaBinaria(int elemento) {
+        int contadorBinario = 0;
+
         int inicio = 0;
-        int fim = vetor.length;
-        while (inicio<vetor[fim-1]){
-            int meio = (inicio+fim)/2;
-            if (vetor[meio]<elemento){
-                inicio = meio;
-            } else{
-                fim = meio;
+        int fim = vetor.length - 1;
+        while (inicio <= fim) {
+            int meio = (inicio + fim) / 2;
+            if (elemento == vetor[meio]) {
+                // contadorBinario++;      //   CORRIGIR O LOCAL ONDE É APLICADO O CONTADOR!!
+                return meio;
+            }
+            if (elemento > vetor[meio]) {
+                inicio = meio + 1;
+            } else {
+                fim = meio - 1;
             }
         }
-        return fim;
+        return contadorBinario;
     }
 
     @Override
